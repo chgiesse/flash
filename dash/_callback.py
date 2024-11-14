9255af37d26f9a4ba57466ebd718d87498ba87b9
Christian Giessel <cgiessel@tesla.com>
2024-11-14 17:52:48 +0100

    Outsourced _invoke_callback to utils

--- dash/_callback.py
+++ dash/_callback.py
@@ -2,11 +2,8 @@ import collections
 import hashlib
 from functools import wraps
 from typing import Callable, Optional, Any
-import inspect
-import warnings
 
 import quart
-from quart.utils import run_sync
 
 from .dependencies import (
     handle_callback_args,
@@ -35,30 +32,12 @@ from ._utils import (
     coerce_to_list,
     AttributeDict,
     clean_property_name,
+    _invoke_callback,
 )
 
 from . import _validate
 from .long_callback.managers import BaseLongCallbackManager
 from ._callback_context import context_value
-
-
-async def _invoke_callback(func, *func_args, **func_kwargs):
-
-    if inspect.iscoroutinefunction(func):
-        output_value = await func(*func_args, **func_kwargs)  # %% callback invoked %%
-
-    else:
-        output_value = await run_sync(func)(
-            *func_args, **func_kwargs
-        )  # %% callback invoked %%
-
-        warnings.warn(
-            f"Function '{func.__name__}' should be a coroutine function (defined with 'async def'). "
-            "While it will still work, this may impact performance and is deprecated.",
-            stacklevel=2,
-        )
-
-    return output_value
 
 
 class NoUpdate:
