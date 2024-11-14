946f1c67f47f7d8e451c1e2fc93a309a170c7bfb
Christian Giessel <cgiessel@tesla.com>
2024-11-14 21:58:52 +0100

    Swapped server runner

--- tests/integration/devtools/test_callback_timing.py
+++ tests/integration/devtools/test_callback_timing.py
@@ -4,7 +4,7 @@ import requests
 from dash import Dash, Output, Input, html, callback_context
 
 
-def test_dvct001_callback_timing(dash_thread_server):
+def test_dvct001_callback_timing(dash_multi_process_server):
     app = Dash(__name__)
     app.layout = html.Div()
 
@@ -14,10 +14,10 @@ def test_dvct001_callback_timing(dash_thread_server):
         sleep(0.5)
         return y
 
-    dash_thread_server(app, debug=True, use_reloader=False, use_debugger=True)
+    dash_multi_process_server(app, debug=True, use_reloader=False, use_debugger=True)
 
     response = requests.post(
-        dash_thread_server.url + "/_dash-update-component",
+        dash_multi_process_server.url + "/_dash-update-component",
         json={
             "output": "x.p",
             "outputs": {"id": "x", "property": "p"},
